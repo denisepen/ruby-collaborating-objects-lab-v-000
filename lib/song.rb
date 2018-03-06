af601744308daca4e0550d4fe1acdba3b7314166
Denise Pendleton <dpendl1@hotmail.com>
2018-03-06 16:06:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,8 @@ class Song
 
 def self.new_by_filename(filename)
   splitname = filename.split(" - ")
-
+  song = self.new(splitname [1])
+  artist = Artist.find_or_create_by_name(splitname[0])
+  song.artist = artist
 end
 end
