2860f50b07e188f0fb9670dfde34dc83de0e0a19
Denise Pendleton <dpendl1@hotmail.com>
2018-03-06 16:10:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,5 +14,7 @@ def self.new_by_filename(filename)
   song = self.new(splitname [1])
   artist = Artist.find_or_create_by_name(splitname[0])
   song.artist = artist
+  artist.add_song(song)
+  song
 end
 end
